c1eb9495a83c956ee765b30cb3f058431b1c8877
Sacha-Élie Ayoun <sachaayoun@gmail.com>
2025-06-03 15:21:18 +0100

    Remove use of Vector
    Signed-off-by: Sacha-Élie Ayoun <sachaayoun@gmail.com>

--- examples/utils.mli
+++ examples/utils.mli
@@ -1,2 +1,2 @@
-val shuffle_vector : _ Vector.t -> unit
+val shuffle_vector : _ Dynarray.t -> unit
 val colour_picker : unit -> unit -> Progress.Color.t
--- src/progress/engine/import.ml
+++ src/progress/engine/import.ml
@@ -12,20 +12,20 @@ module Mtime = struct
   let span_to_s span = Mtime.Span.to_float_ns span *. 1e-9
 end
 
-module Vector = struct
-  include Vector
+module Dynarray = struct
+  include Dynarray
 
   let iter ~f t = iter f t
 
   let iteri_from ~f i t =
     for i = i to length t - 1 do
-      f i (unsafe_get t i)
+      f i (get t i)
     done
 
   let rec find_map_from i t ~f =
     if i >= length t then None
     else
-      let a = unsafe_get t i in
+      let a = get t i in
       match f a with
       | Some _ as some -> some
       | None -> find_map_from (i + 1) t ~f
@@ -33,17 +33,17 @@ module Vector = struct
   let find_map t ~f = find_map_from 0 t ~f
 
   let insert t k v =
-    Vector.push t v (* Dummy insertion to expand *);
-    for i = Vector.length t - 1 downto k + 1 do
-      Vector.set t i (Vector.get t (pred i))
+    Dynarray.add_last t v (* Dummy insertion to expand *);
+    for i = Dynarray.length t - 1 downto k + 1 do
+      Dynarray.set t i (Dynarray.get t (pred i))
     done;
-    Vector.set t k v
+    Dynarray.set t k v
 
   let remove (type a) (t : a t) k =
-    for i = k to Vector.length t - 2 do
-      Vector.set t i (Vector.get t (succ i))
+    for i = k to Dynarray.length t - 2 do
+      Dynarray.set t i (Dynarray.get t (succ i))
     done;
-    ignore (Vector.pop t : a)
+    ignore (Dynarray.pop_last t : a)
 
   let get_exn = get
   let get = `shadowed
